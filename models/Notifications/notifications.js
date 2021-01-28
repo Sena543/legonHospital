fd716fa853e3d4a90a65cc9d1bd96121e7868c5c
Sena <senanuazanu@gmail.com>
2021-01-28 19:05:55 +0000

    notifications schema

---
+++ models/Notifications/notifications.js
@@ -0,0 +1,16 @@
+const { Schema, model, SchemaTypes } = require("mongoose");
+
+let schema = new Schema({
+	// ID of the user this notification belongs to.
+	userID: { type: SchemaTypes.ObjectId, ref: "Student" },
+	notifications: [
+		new Schema({
+			date: Date,
+			title: String,
+			message: String,
+			seen: Boolean,
+		}),
+	],
+});
+
+module.exports = notificationsData = model("Notifications", schema);
