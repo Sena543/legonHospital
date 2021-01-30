9805cccb505107c866e3f69a203cb02bce00f760
Sena <senanuazanu@gmail.com>
2021-01-30 23:49:56 +0000

    login model

--- models/Login/Login.js
+++ models/Login/Login.js
@@ -1,31 +1,27 @@
-import mongoose from "mongoose";
-import bcrypt from 'bcrypt';
+const { Schema, model } = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const loginSchema = new mongoose.Schema({
-    staffID: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
+const loginSchema = new Schema({
+	staffID: {
+		type: String,
+		required: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+});
 
-loginSchema.pre('save', function (next) {
-    let staff = this
-    bcrypt.genSalt(10, function (err, salt) {
-        if (err) return next(err)
-        bcrypt.hash(staff.password, salt, function (err, hash) {
-            if (err) return next(err)
-            staff.password = hash;
-            next()
-        });
-    });
-})
+loginSchema.pre("save", function (next) {
+	let staff = this;
+	bcrypt.genSalt(10, function (err, salt) {
+		if (err) return next(err);
+		bcrypt.hash(staff.password, salt, function (err, hash) {
+			if (err) return next(err);
+			staff.password = hash;
+			next();
+		});
+	});
+});
 
-// loginSchema.method("validatePasswords", async function(){
-//     return await bcrypt.compare(password, _login.password)
-// })
-
-export default mongoose.model('Login', loginSchema,)
+module.exports = loginModel = model("Login", loginSchema);
